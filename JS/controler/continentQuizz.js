7d0ca9a594a6de1111a178f55ea083258f2285ad
Nuno Sousa <9180579@esmad.ipp.pt>
2019-05-28 23:45:30 +0100

    yez

--- JS/controler/continentQuizz.js
+++ JS/controler/continentQuizz.js
@@ -1,11 +1,39 @@
-window.onload = function() {
-    if (sessionStorage.getItem('africa')) {
-        africa = JSON.parse(sessionStorage.getItem('africa'))
+window.onload = function () {
+    if (sessionStorage.getItem('continentStyle')) {
+        continentStyle = JSON.parse(sessionStorage.getItem('continentStyle'))
     }
 
-    if (africa == 'africa') {
-        document.body.style.backgroundColor = '#4AB71B'
+    styleChangeByContinent()
+}
+
+function styleChangeByContinent() {
+    if (continentStyle == 'africa') {
+        document.body.style.backgroundColor = '#F5BB00'
         document.querySelector('#continentHeader').innerHTML = 'África'
         document.querySelector('#continentHeader').style.color = '#FFFFFF'
-    } 
+    }
+
+    if (continentStyle == 'america'){
+        document.body.style.backgroundColor = '#4AB71B'
+        document.querySelector('#continentHeader').innerHTML = 'América'
+        document.querySelector('#continentHeader').style.color = '#FFFFFF'
+    }
+
+    if (continentStyle == 'asia'){
+        document.body.style.backgroundColor = '#BE3DE3'
+        document.querySelector('#continentHeader').innerHTML = 'Ásia'
+        document.querySelector('#continentHeader').style.color = '#FFFFFF'
+    }
+
+    if (continentStyle == 'europa'){
+        document.body.style.backgroundColor = '#204987'
+        document.querySelector('#continentHeader').innerHTML = 'Europa'
+        document.querySelector('#continentHeader').style.color = '#FFFFFF'
+    }
+
+    if (continentStyle == 'oceania'){
+        document.body.style.backgroundColor = '#B7541B'
+        document.querySelector('#continentHeader').innerHTML = 'Oceânia'
+        document.querySelector('#continentHeader').style.color = '#FFFFFF'
+    }
 }
--- JS/controler/quizz.js
+++ JS/controler/quizz.js
@@ -1,7 +1,31 @@
-let africa = ""
+let continentStyle = ""
 
 document.querySelector('#africanBtnQuiz').addEventListener('click', function() {
-    africa = 'africa'
-    sessionStorage.setItem('africa', JSON.stringify(africa))
-    location.href = '../../HTML/continentQuizz.html'
+    continentStyle = 'africa'
+    addValue(continentStyle)
 })
+
+document.querySelector('#americanBtnQuiz').addEventListener('click', function() {
+    continentStyle = 'america'
+    addValue(continentStyle)
+})
+
+document.querySelector('#asianBtnQuiz').addEventListener('click', function() {
+    continentStyle = 'asia'
+    addValue(continentStyle)
+})
+
+document.querySelector('#europeanBtnQuiz').addEventListener('click', function() {
+    continentStyle = 'europa'
+    addValue(continentStyle)
+})
+
+document.querySelector('#oceaniaBtnQuiz').addEventListener('click', function() {
+    continentStyle = 'oceania' 
+    addValue(continentStyle)  
+})
+
+function addValue(continentStyle) {
+    sessionStorage.setItem('continentStyle', JSON.stringify(continentStyle))
+    location.href = '../../HTML/continentQuizz.html'
+}
