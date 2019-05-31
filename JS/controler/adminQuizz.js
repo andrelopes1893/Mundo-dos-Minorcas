1dd6321030451cc4338d4fbdc541ded0fecd57aa
Nuno Sousa <9180579@esmad.ipp.pt>
2019-05-31 01:19:25 +0100

    yez

--- JS/controler/adminQuizz.js
+++ JS/controler/adminQuizz.js
@@ -8,6 +8,30 @@ window.onload = function () {
     }
 }
 let answers = []
+if (document.getElementById('quizCreateForm') != null) {
+    document.getElementById('quizCreateForm').addEventListener('submit', function (event) {
+        let quizContinent = document.querySelector('#stlContinent').value
+        let quizCategory = document.querySelector('#stlCategory').value
+        let level = document.querySelector('#stlLevel').value
+        let txtWrongAnswer1 = document.querySelector('#txtAnswerOptions1').value
+        let txtWrongAnswer2 = document.querySelector('#txtAnswerOptions2').value
+        let txtWrongAnswer3 = document.querySelector('#txtAnswerOptions3').value
+        let txtCorrectAnswer = document.querySelector('#txtCorrectAnswer').value
+        console.log(Question.ConfirmQuizExistence(quizContinent, quizCategory, txtCorrectAnswer))
+
+        if (Question.ConfirmQuizExistence(quizContinent, quizCategory, txtCorrectAnswer)) {
+            alert("O quiz já existente ")
+        } else {
+            answers.push(txtWrongAnswer1, txtWrongAnswer2, txtWrongAnswer3, txtCorrectAnswer)
+            quizzes.push(new Question(quizCategory, quizContinent, level, Question.establishQuizQuestion(quizCategory), answers, 4, Question.xpByLevel(level)))
+            localStorage.setItem('quizzes', JSON.stringify(quizzes))
+            answers = []
+        }
+
+        event.preventDefault()
+    })
+}
+
 document.getElementById('quizCreateForm').addEventListener('submit', function (event) {
     let quizContinent = document.querySelector('#stlContinent').value
     let quizCategory = document.querySelector('#stlCategory').value
@@ -18,39 +42,16 @@ document.getElementById('quizCreateForm').addEventListener('submit', function (e
     let txtCorrectAnswer = document.querySelector('#txtCorrectAnswer').value
     console.log(Question.ConfirmQuizExistence(quizContinent, quizCategory, txtCorrectAnswer))
 
-    if (Question.ConfirmQuizExistence(quizContinent, quizCategory, txtCorrectAnswer)) {
-        alert("O quiz já existente ")
-    } else {
-        answers.push(txtWrongAnswer1, txtWrongAnswer2, txtWrongAnswer3, txtCorrectAnswer)
-        quizzes.push(new Question(quizCategory, quizContinent, level, Question.establishQuizQuestion(quizCategory), answers,4, Question.xpByLevel(level)))
-        localStorage.setItem('quizzes', JSON.stringify(quizzes))
-        answers = []
-    }
-
-    event.preventDefault()
-})
-
-document.getElementById('quizCreateForm').addEventListener('submit', function (event) {
-    let quizContinent = document.querySelector('#stlContinent').value
-    let quizCategory = document.querySelector('#stlCategory').value
-    let level = document.querySelector('#stlLevel').value
-    let txtWrongAnswer1 = document.querySelector('#txtAnswerOptions1').value
-    let txtWrongAnswer2 = document.querySelector('#txtAnswerOptions2').value
-    let txtWrongAnswer3 = document.querySelector('#txtAnswerOptions3').value
-    let txtCorrectAnswer = document.querySelector('#txtCorrectAnswer').value
-    console.log(Question.ConfirmQuizExistence(quizContinent, quizCategory, txtCorrectAnswer))
-
 
 
     if (Question.ConfirmQuizExistence(quizContinent, quizCategory, txtCorrectAnswer)) {
         alert("O quiz já existente ")
     } else {
         answers.push(txtWrongAnswer1, txtWrongAnswer2, txtWrongAnswer3, txtCorrectAnswer)
-        quizzes.push(new Question(quizCategory, quizContinent, level, Question.establishQuizQuestion(quizCategory), answers,3, Question.xpByLevel(level)))
+        quizzes.push(new Question(quizCategory, quizContinent, level, Question.establishQuizQuestion(quizCategory), answers, 3, Question.xpByLevel(level)))
         localStorage.setItem('quizzes', JSON.stringify(quizzes))
         answers = []
     }
 
     event.preventDefault()
 })
->>>>>>> 6aec1fe8ff30a99c927012ef640ef30a78863ed4
