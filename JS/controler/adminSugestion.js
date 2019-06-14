3eb1d8b374ecfac3fc61dcce2acefee668b8a78c
Nuno Sousa <9180579@esmad.ipp.pt>
2019-06-14 17:03:50 +0100

    yez

--- JS/controler/adminSugestion.js
+++ JS/controler/adminSugestion.js
@@ -36,46 +36,49 @@ function renderTable() {
                                             <td><button type="button" id="" data-toggle="modal" data-target="#acceptSuggestion" class="btn acceptSuggestion"><img src="/Images/lock.png" alt="Bloquear"></button></td>
                                         </tr>`
     }
-
     // removeButtons()
     openModals()
 }
 
 function openModals() {
-    let openBtns = document.getElementsByClassName('btn open')
+    let openBtns = document.getElementsByClassName('open')
     for (const elem of openBtns) {
-        elem.addEventListener('click', function () {
+        elem.addEventListener('click', function () {            
             renderSuggestionModal(this.id)
             console.log(this.id + '-' + renderSuggestionModal(this.id));
         })
     }
 }
-console.log(renderSuggestionModal(this.id)); //undefined
 
 function renderSuggestionModal(id) {
     for (const suggestion of suggestions) {
         if (suggestion._id == id) {
             console.log(suggestion._id + ' - ' + id);
+
+            document.querySelector('#txtContinent').innerHTML = 'Continente: ' + suggestion._continent
+            document.querySelector('#txtCountry').innerHTML = 'País: ' + suggestion._country
+            document.querySelector('#txtCapital').innerHTML = 'Capital: ' + suggestion._capital
+            document.querySelector('#txtLanguage').innerHTML = 'Língua: ' + suggestion._language
             
-            document.body.innerHTML += `<div class="modal fade" id="suggestionModal">
-                                        <div class="modal-dialog modal-lg modal-dialog-centered">
-                                            <div class="modal-content">
-                                                <!-- Cabeçalho da Modal -->
-                                                <div class="modal-header">
-                                                    <img src="/Images/Logo2.png" class="modalLogo" alt="Logo">
-                                                    <button type="button" class="btn btn-danger" data-dismiss="modal"
-                                                        style="color:#ffff">Voltar</button>
-                                                </div>
-                                                <!-- Corpo da Modal -->
-                                                <div class="modal-body text-center suggestion">
-                                                    <p class="lead">Continente: ${suggestion._continent}</p>
-                                                    <p class="lead">País: ${suggestion._country}</p>
-                                                    <p class="lead">Capital: ${suggestion._capital}</p>
-                                                    <p class="lead">Línguas Oficiais: ${suggestion._language}</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>`
+            // document.body.innerHTML += `<div class="modal fade" id="suggestionModal">
+            //                             <div class="modal-dialog modal-lg modal-dialog-centered">
+            //                                 <div class="modal-content">
+            //                                     <!-- Cabeçalho da Modal -->
+            //                                     <div class="modal-header">
+            //                                         <img src="/Images/Logo2.png" class="modalLogo" alt="Logo">
+            //                                         <button type="button" class="btn btn-danger" data-dismiss="modal"
+            //                                             style="color:#ffff">Voltar</button>
+            //                                     </div>
+            //                                     <!-- Corpo da Modal -->
+            //                                     <div class="modal-body text-center suggestion">
+            //                                         <p class="lead">Continente: ${suggestion._continent}</p>
+            //                                         <p class="lead">País: ${suggestion._country}</p>
+            //                                         <p class="lead">Capital: ${suggestion._capital}</p>
+            //                                         <p class="lead">Línguas Oficiais: ${suggestion._language}</p>
+            //                                     </div>
+            //                                 </div>
+            //                             </div>
+            //                         </div>`
         }
     }
 }
