40798a78124f771079c103c683e14c74b04d0b6b
Tiago De Pina <tiagopina20014@gmail..com>
2019-06-18 15:42:01 -0100

    paginacao

--- JS/controler/adminCatalog.js
+++ JS/controler/adminCatalog.js
@@ -57,30 +57,34 @@ function renderTable() {
     if (localStorage.countries) {
         countries = JSON.parse(localStorage.countries)
     }
+    if (document.querySelector('#stlOrder')) {
 
-    if (document.querySelector('#stlOrder').value == "Ordem Alfabetica Crescente dos Continentes") {        
-        countries.sort(Country.ascendentContinentOrder)
+        if (document.querySelector('#stlOrder').value == "Ordem Alfabetica Crescente dos Continentes") {
+            countries.sort(Country.ascendentContinentOrder)
+        }
+
+        if (document.querySelector('#stlOrder').value == "Ordem Alfabetica Decrescente dos Continentes") {
+            countries.sort(Country.descendentContinentOrder)
+        }
+        document.querySelector('#countriesTableBody').innerHTML = ''
+
+        let r = 0
+        for (const country of countries) {
+            r++
+            document.querySelector('#countriesTableBody').innerHTML += `<tr>
+                                                                            <th scope="row">${r}</th>
+                                                                            <td>${country._continent}</td>
+                                                                            <td>${country._name}</td>
+                                                                            <td><button type="button" id="${country._id}" data-toggle="modal" data-target="#ModalEditCountry" class="btn editBtn"><img src="/Images/lock.png" alt="Editar"></button></td> 
+                                                                            <td><button type="button" id="${country._name}" class="btn remove"><img src="/Images/x.png" alt="Remover"></button></td>
+                                                                        </tr> `
+        }
+        removeButtons()
+        editButtons()
+
+
     }
-
-    if (document.querySelector('#stlOrder').value == "Ordem Alfabetica Decrescente dos Continentes") {
-        countries.sort(Country.descendentContinentOrder)
-    }
-
-    document.querySelector('#countriesTableBody').innerHTML = ''
-
-    let r = 0
-    for (const country of countries) {
-        r++
-        document.querySelector('#countriesTableBody').innerHTML += `<tr>
-                                                                        <th scope="row">${r}</th>
-                                                                        <td>${country._continent}</td>
-                                                                        <td>${country._name}</td>
-                                                                        <td><button type="button" id="${country._id}" data-toggle="modal" data-target="#ModalEditCountry" class="btn editBtn"><img src="/Images/lock.png" alt="Editar"></button></td> 
-                                                                        <td><button type="button" id="${country._name}" class="btn remove"><img src="/Images/x.png" alt="Remover"></button></td>
-                                                                    </tr> `
-    }
-    removeButtons()
-    editButtons()
+  
 }
 
 function removeButtons() {
--- JS/controler/countries.js
+++ JS/controler/countries.js
@@ -6,14 +6,19 @@ let users = []
 let ratings = []
 let countryId
 let currentCountry
+let continentCountrys = []
 
+motherFunction()
 //o que acontece quando a paginas que usam com suposto este ficheiro, são carregadas
-window.onload = function () {
+
+function motherFunction() {
+
     // if (localStorage.countries) {
     //     countries = JSON.parse(localStorage.countries)
     // }
+   
     if (localStorage.getItem("countries")) {
-        countries = JSON.parse(localStorage.getItem("users"))
+        countries = JSON.parse(localStorage.getItem("countries"))
     }
 
     if (localStorage.getItem("users")) {
@@ -23,18 +28,24 @@ window.onload = function () {
         localStorage.removeItem('page')
     }
 
-   
+    continentCountrysFill()
+
+
 
 
     renderCatalog()
     // !!!new
-    ConstructPaginationButton() 
+    ConstructPaginationButton()
     renderModalInfo()
     userData()
     if (sessionStorage.getItem('continentCatalogStyle')) {
         continentCatalogStyle = sessionStorage.getItem('continentCatalogStyle')
     }
+
+
+
 }
+
 
 searchCountry()
 
@@ -49,13 +60,17 @@ if (document.querySelector("#btnFilter") != null) {
 
 //função que renderiza o catalogo
 function renderCatalog(quantity = 16) {
-    if (document.querySelector('#stlGenre').value == "Ordem Alfabetica Crescente") {
-        sortCountriesAscendent()
+    if(document.querySelector('#stlGenre')){
+        if (document.querySelector('#stlGenre').value == "Ordem Alfabetica Crescente") {
+            sortCountriesAscendent()
+        }
+    
+        if (document.querySelector('#stlGenre').value == "Ordem Alfabetica Decrescente") {
+            sortCountriesDescendent()
+        }
     }
+    
 
-    if (document.querySelector('#stlGenre').value == "Ordem Alfabetica Decrescente") {
-        sortCountriesDescendent()
-    }
 
     if (quantity == 'prev' || quantity == 'next') {
         let value = 16;
@@ -63,6 +78,17 @@ function renderCatalog(quantity = 16) {
             if (JSON.parse(localStorage.getItem('page'))) {
                 if (JSON.parse(localStorage.getItem('page')) == 16) {
                     value = 0
+                    const usernameToast = Swal.mixin({
+                        toast: true,
+                        position: 'bottom-end',
+                        showConfirmButton: false,
+                        timer: 3000,
+                        background: '#29ABE2'
+                    });
+                    usernameToast.fire({
+                        type: 'info',
+                        title: '<span style="color:#FFFFFF">Já estás na primeira página<span>'
+                    })
 
                 }
                 quantity = Number(JSON.parse(localStorage.getItem('page'))) - value
@@ -74,7 +100,7 @@ function renderCatalog(quantity = 16) {
             localStorage.setItem('page', JSON.stringify(quantity))
         } else {
             if (JSON.parse(localStorage.getItem('page'))) {
-                if (JSON.parse(localStorage.getItem('page')) >= a.length) {
+                if (JSON.parse(localStorage.getItem('page')) >= continentCountrys.length) {
                     value = 0
                     const usernameToast = Swal.mixin({
                         toast: true,
@@ -91,6 +117,11 @@ function renderCatalog(quantity = 16) {
                 quantity = Number(JSON.parse(localStorage.getItem('page'))) + value
             } else {
                 quantity = 32
+
+                if(continentCountrys[32-16]==null){
+                    quantity=32-16
+
+                }
             }
             localStorage.setItem('page', JSON.stringify(quantity))
         }
@@ -100,49 +131,54 @@ function renderCatalog(quantity = 16) {
 
 
 
+
+
     let result = ""
     let i = 0
-    let contName = document.querySelector('.continentTitle').innerHTML
+    // let contName = document.querySelector('.continentTitle').innerHTML
 
 
-    if(countries.length>0){
+    if (continentCountrys.length > 0) {
         for (let j = Number(quantity) - 16; j < Number(quantity); j++) {
 
-            if (removeAcento(contName.toLowerCase()) == countries[j]._continent.toLowerCase()) {
-                if (i % 4 === 0) {
-                    result += `<div class="row">`
-                }
-                result += `
-                    <div class="col-lg-3 col-sm-6 col-xs-12" id="countriesCol">
-                        <div class="card africanCards" style="width: 18rem;">
-                        <button type="button" id="${countries[j]._id}" class="btn countryButton" data-toggle="modal" data-target="#countryModal">
-                            <img src="${countries[j]._flag}" class="card-img-top" alt="Brasil">
-                        </button>
-                            <div class="card-body" id="${countries[j]._id}">
-                                <p class="card-text paragraph">NOME: <span>${countries[j]._name}</span> </p>
-                                <p class="card-text paragraph">CAPITAL: <span>${countries[j]._capital}</span></p>
-                                <p class="card-text paragraph">LÍNGUA: <span>${countries[j]._language}</span></p>
-                                <div class="stars-outer">
-                                    <div class="stars-inner"></div>
-                                </div>
+
+            if (i % 4 === 0) {
+                result += `<div class="row">`
+            }
+            result += `
+                <div class="col-lg-3 col-sm-6 col-xs-12" id="countriesCol">
+                    <div class="card africanCards" style="width: 18rem;">
+                    <button type="button" id="${continentCountrys[j]._id}" class="btn countryButton" data-toggle="modal" data-target="#countryModal">
+                        <img src="${continentCountrys[j]._flag}" class="card-img-top" alt="Brasil">
+                    </button>
+                        <div class="card-body" id="${continentCountrys[j]._id}">
+                            <p class="card-text paragraph">NOME: <span>${continentCountrys[j]._name}</span> </p>
+                            <p class="card-text paragraph">CAPITAL: <span>${continentCountrys[j]._capital}</span></p>
+                            <p class="card-text paragraph">LÍNGUA: <span>${continentCountrys[j]._language}</span></p>
+                            <div class="stars-outer">
+                                <div class="stars-inner"></div>
                             </div>
                         </div>
-                    </div>`
-                i++
-    
-                if (i % 4 === 0) {
-                    result += `</div>`
-                }
-    
-                if ((i + 1) == countries.length) {
-                    break;
-                 }
+                    </div>
+                </div>`
+            i++
+
+            if (i % 4 === 0) {
+                result += `</div>`
             }
+
+            if ((j + 1) == continentCountrys.length) {
+                break;
+            }
+
+            // if (removeAcento(contName.toLowerCase()) ==continentCountrys[j]._continent.toLowerCase()) {
+
+            // }
         }
 
     }
 
-    
+
 
 
 
@@ -180,7 +216,11 @@ function renderCatalog(quantity = 16) {
     // }
     ratingButtons()
 
-    document.querySelector("#containerCatalog").innerHTML = result
+    if(document.querySelector("#containerCatalog")!=null){
+        document.querySelector("#containerCatalog").innerHTML = result
+    }
+
+   
 
     // Programar botoes nas imagens dos modais
     let countryBtn = document.getElementsByClassName("countryButton")
@@ -233,12 +273,12 @@ export function removeAcento(text) {
 
 //Função que ordena o array de paises pelo nome, de forma crescente, no container
 function sortCountriesAscendent() {
-    countries.sort(Country.ascendentAlphabeticOrder)
+    continentCountrys.sort(Country.ascendentAlphabeticOrder)
 }
 
 //Função que ordena o array de paises pelo nome, de forma decrescente, no container
 function sortCountriesDescendent() {
-    countries.sort(Country.descendentAlphabeticOrder)
+    continentCountrys.sort(Country.descendentAlphabeticOrder)
 }
 
 //funçao para procurar paises 
@@ -351,44 +391,68 @@ function userData() {
 function ConstructPaginationButton() {
 
 
-    let quantity = countries.length / 16
+    let quantity = continentCountrys.length / 16
     let quantityTostring = String(quantity)
     let contains = false
- 
- 
- 
+
+
+
     for (let i = 0; i < quantityTostring.length; i++) {
-       if (quantityTostring[i] == '.') {
-          contains = true
-       }
+        if (quantityTostring[i] == '.') {
+            contains = true
+        }
     }
- 
+
     if (contains == true) {
-       quantity = parseInt(quantity) + 1
-    } 
-    else {
-       quantity = quantity
+        quantity = parseInt(quantity) + 1
+    } else {
+        quantity = quantity
     }
- 
-    let paginationHolder = document.querySelector('.pagination')
-    paginationHolder.innerHTML = ``
-    paginationHolder.innerHTML += `<li class="page-item"><button id='prev'class="page-link">Prev</button></li>`
-    for (let i = 1; i <= quantity; i++) {
-       paginationHolder.innerHTML += `<li class="page-item"><button  id='${i*16}'class="page-link">${i}</button></li>`
+    if (quantity < 1) {
+        quantity = 1
     }
-    paginationHolder.innerHTML += `<li class="page-item"><button id="next"class="page-link">Next</button></li>`
-    getPageSelection()
- }
- 
- 
- function getPageSelection() {
+    if (document.querySelector('.pagination')) {
+        let paginationHolder = document.querySelector('.pagination')
+        paginationHolder.innerHTML = ``
+        paginationHolder.innerHTML += `<li class="page-item"><button id='prev'class="page-link">Prev</button></li>`
+        for (let i = 1; i <= quantity; i++) {
+            paginationHolder.innerHTML += `<li class="page-item"><button  id='${i*16}'class="page-link">${i}</button></li>`
+        }
+        paginationHolder.innerHTML += `<li class="page-item"><button id="next"class="page-link">Next</button></li>`
+        getPageSelection()
+
+    }
+
+}
+
+
+function getPageSelection() {
     let pagesLinks = document.querySelectorAll(".page-link")
     for (const button of pagesLinks) {
-       button.addEventListener('click', function () {
-          if (this.id != 'prev' && this.id != 'next') {
-             localStorage.setItem('page', JSON.stringify(this.id))
-          }
-          renderCatalog(this.id)
-       })
+        button.addEventListener('click', function () {
+            if (this.id != 'prev' && this.id != 'next') {
+                localStorage.setItem('page', JSON.stringify(this.id))
+            }
+            renderCatalog(this.id)
+        })
     }
- }
+}
+
+
+
+
+function continentCountrysFill() {
+    if(document.querySelector('.continentTitle')){
+        let contName = document.querySelector('.continentTitle').innerHTML
+        for (const country of countries) {
+            if (removeAcento(contName.toLowerCase()) == country._continent.toLowerCase()) {
+                continentCountrys.push(country)
+            }
+        }
+
+    }
+
+    
+
+ 
+}
